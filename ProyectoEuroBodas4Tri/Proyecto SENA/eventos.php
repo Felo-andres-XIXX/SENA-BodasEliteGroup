4ed49bcd0e7d985c372fbc0b52b06f6cc490a1e1
Andres-Cruz-19 <andrescifuentes799@gmail.com>
2020-02-20 17:32:14 -0500

    Archivos del proyecto V0.1
    Se sube o agrega la parte de php, css, js, bd y etc..

---
+++ ProyectoEuroBodas4Tri/Proyecto SENA/eventos.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html>
+ <title>Eurobodas </title>
+ <!-- Estilos chingue su madre -->
+
+
+
+ <head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="keywords" content="footer, address, phone, icons" />
+ 
+	<title>Responsive Footer</title>
+ 
+	<link rel="stylesheet" href="style.css">
+	
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+ 
+    <link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
+    
+    <link href="CSS/estilos.css" rel="stylesheet" type="text/css">
+ 
+</head>
+
+	
+<!DOCTYPE html>
+
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Hasta aqui no mas -->
+
+
+<body>
+
+<div class="topnav">
+  <a href="nosotros.html">Nosotros</a>
+  <a href="Registro.html">Registro</a>
+  <div class="search-container">
+    <form action="/action_page.php">
+      <input type="text" placeholder="Search.." name="search">
+      <button type="submit">Buscar</button>
+    </form>
+  </div>
+</div>
+
+<div align="center">
+
+  <img src="elite.png" style="width: 20%">
+
+</div>
+
+
+<div class="btn-group" style="width:100%">
+  <a href="index.html"><button style="width:16.663%">Inicio</button></a>
+  <a href="eventos.html"><button style="width:16.663%">Eventos</button></a>
+  <a href="menu.html"><button style="width:16.663%">Menu</button></a>
+  <a href="Servicios.html"><button style="width:16.663%">Servicios</button></a>
+  <a href="login.html"><button style="width:16.663%">Iniciar Sesion</button></a>
+  <a href="contacto.html"><button style="width:16.663%">Contacto</button></a>
+</div>
+
+<!-- Parte a editar -->
+<br></br>
+
+	
+  <p class="heading">Eventos</p>
+  <div class="gallery-image">
+    <div class="img-box">
+      <img src="quinces.jpg" alt="" />
+      <div class="transparent-box">
+        <div class="caption">
+          <p>15 años</p>
+          <p class="opacity-low">Reuniones de 15 años</p>
+        </div>
+      </div> 
+    </div>
+    <div class="img-box">
+      <img src="boda.jpg" alt="" />
+      <div class="transparent-box">
+        <div class="caption">
+          <p>Matrimonios</p>
+          <p class="opacity-low">Ceremonias matrimoniales</p>
+        </div>
+      </div>
+    </div>
+    <div class="img-box">
+      <img src="Empresa.jpg" alt="" />
+      <div class="transparent-box">
+        <div class="caption">
+          <p>Eventos Empresariales</p>
+          <p class="opacity-low">Eventos de todo tipo para su empresa</p>
+        </div>
+      </div>
+    </div>
+    <div class="img-box">
+      <img src="grado.jpg" alt="" />
+      <div class="transparent-box">
+        <div class="caption">
+          <p>Grados</p>
+          <p class="opacity-low">Grados para instituciones educativas</p>
+        </div>
+      </div> 
+    </div>
+    <div class="img-box">
+      <img src="fiestatem.jpg" alt="" />
+      <div class="transparent-box">
+        <div class="caption">
+          <p>Fiestas tematicas</p>
+          <p class="opacity-low">Fiestas personalizadas con cualquier tipo de decoracion a discutir</p>
+        </div>
+      </div> 
+    </div>
+      </div> 
+    </div>
+  </div>
+
+<!-- Fin de la parte a editar -->
+<br></br>
+<br></br>
+
+ 
+<footer class="footer-distributed">
+ 
+			<div class="footer-left">
+          <img src="elite.png" style="width: 18%">
+				<h3>Euro<span>Bodas</span></h3>
+ 
+				<p class="footer-links">
+					<a href="index.html">Inicio</a>
+					|
+					<a href="nosotros.html">Sobre Nosotros</a>
+					|
+					<a href="#">About</a>
+					|
+					<a href="#">Contacto</a>
+				</p>
+ 
+				<p class="footer-company-name">© 2019 EuroBodas Derechos reservados </p>
+			</div>
+ 
+			<div class="footer-center">
+				<div>
+					<i class="fa fa-map-marker"></i>
+					  <p><span>a 22b-71,, Cra. 44a #22b1</span>
+						Colombia, Bogota - 110111</p>
+				</div>
+ 
+				<div>
+					<i class="fa fa-phone"></i>
+					<p>+57 321 477 1511</p>
+				</div>
+				<div>
+					<i class="fa fa-envelope"></i>
+					<p><a href="mailto:comercial01.bodaselite@gmail.com">comercial01.bodaselite@gmail.com</a></p>
+
+
+				</div>
+			</div>
+			<div class="footer-right">
+				<p class="footer-company-about">
+					<span>Mas de nosotros</span>
+					Ofrecemos todo tipo de eventos, sociales, empresariales, y todo lo referente a cada uno.</p>
+				<div class="footer-icons">
+					<a href="#"><i class="fa fa-facebook"></i></a>
+					<a href="#"><i class="fa fa-instagram"></i></a>
+					<a href="#"><i class="fa fa-linkedin"></i></a>
+					<a href="#"><i class="fa fa-youtube"></i></a>
+				</div>
+			</div>
+		</footer>
+ 
+</body>
+
+</html>
