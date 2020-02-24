dcd15cdd989e3dbb0b2877211d7076bd630330f6
Camilo urrego <51963457+camilu0802@users.noreply.github.com>
2020-02-24 15:26:24 -0500

    Add files via upload

---
+++ Nosotros.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html>
+ <title>Nosotros </title>
+ 
+ <head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="keywords" content="footer, address, phone, icons" />
+ 
+	<title>Responsive Footer</title>
+ 
+	<link rel="stylesheet" href="css/estilo.css">
+	
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+ 
+	<link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
+
+	<link href="css/estilo.css" rel="stylesheet" type="text/css">
+ 
+</head>
+
+
+<body>	
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+</head>
+<body>
+
+<div class="topnav">
+  <a href="nosotros.php">Nosotros</a>
+  <a href="Registro.php">Registro</a>
+  <div class="search-container">
+    <form action="/action_page.php">
+      <input type="text" placeholder="Search.." name="search">
+      <button type="submit">Buscar</button>
+    </form>
+  </div>
+</div>
+
+<div align="center">
+
+  <img src="img/elite.png" style="width: 20%">
+
+</div>
+
+<div class="btn-group" style="width:100%">
+  <a href="index.php"><button style="width:16.663%">Inicio</button></a>
+  <a href="eventos.php"><button style="width:16.663%">Eventos</button></a>
+  <a href="menu.php"><button style="width:16.663%">Menu</button></a>
+  <a href="servicios.php"><button style="width:16.663%">Servicios</button></a>
+  <a href="login.php"><button style="width:16.663%">Iniciar Sesion</button></a>
+  <a href="contacto.php"><button style="width:16.663%">Contacto</button></a>
+</div>
+
+
+<br></br>
+
+<div id="header" align="center">
+  <a id="logo"><h1>¿Quienes somos?</h1></a>
+  <br></br>
+ <div class="form">
+		<h3>Somos una empresa organizadora y productora de eventos sociales y empresariales creada hace año y medio pero con una experiencia de 20 años de los socios. Se especializa en  Bodas 15 años, cumpleaños , despedidas de fin de año y eventos de empresas y nuestro proposito es la satisfaccion total de nuestros clientes.</h3>
+
+	</div> <img src="img/ev.jpg" style="width: 50%">
+
+
+</div>
+
+<!-- Fin de la parte a editar -->
+
+ 
+<footer class="footer-distributed">
+ 
+			<div class="footer-left">
+          <img src="img/elite.png" style="width: 18%">
+				<h3>Euro<span>Bodas</span></h3>
+ 
+				<p class="footer-links">
+					<a href="index.php">Inicio</a>
+					|
+					<a href="nosotros.php">Sobre Nosotros</a>
+					|
+					<a href="#">About</a>
+					|
+					<a href="#">Contacto</a>
+				</p>
+ 
+				<p class="footer-company-name">© 2019 EuroBodas Derechos reservados </p>
+			</div>
+ 
+			<div class="footer-center">
+				<div>
+					<i class="fa fa-map-marker"></i>
+					  <p><span>a 22b-71,, Cra. 44a #22b1</span>
+						Colombia, Bogota - 110111</p>
+				</div>
+ 
+				<div>
+					<i class="fa fa-phone"></i>
+					<p>+57 321 477 1511</p>
+				</div>
+				<div>
+					<i class="fa fa-envelope"></i>
+					<p><a href="mailto:comercial01.bodaselite@gmail.com">comercial01.bodaselite@gmail.com</a></p>
+
+
+				</div>
+			</div>
+			<div class="footer-right">
+				<p class="footer-company-about">
+					<span>Mas de nosotros</span>
+					Ofrecemos todo tipo de eventos, sociales, empresariales, y todo lo referente a cada uno.</p>
+				<div class="footer-icons">
+					<a href="#"><i class="fa fa-facebook"></i></a>
+					<a href="#"><i class="fa fa-instagram"></i></a>
+					<a href="#"><i class="fa fa-linkedin"></i></a>
+					<a href="#"><i class="fa fa-youtube"></i></a>
+				</div>
+			</div>
+		</footer>
+
+</body>
+
+</html>
---
+++ menu.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html>
+ <title>Eurobodas </title>
+ <!-- Estilos chingue su madre -->
+
+
+
+ <head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="keywords" content="footer, address, phone, icons" />
+ 
+	<title>Responsive Footer</title>
+ 
+	<link rel="stylesheet" href="css/estilo.css">
+	
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+ 
+  <link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
+  
+  <link href="css/estilo.css" rel="stylesheet" type="text/css">
+</head>
+
+
+<!-- Hasta aqui no mas -->
+
+</head>
+<body>
+
+<div class="topnav">
+  <a href="nosotros.php">Nosotros</a>
+  <a href="Registro_cliente.php">Registro</a>
+  <div class="search-container">
+    <form action="/action_page.php">
+      <input type="text" placeholder="Search.." name="search">
+      <button type="submit">Buscar</button>
+    </form>
+  </div>
+</div>
+
+<div align="center">
+
+  <img src="img/elite.png" style="width: 20%">
+
+</div>
+
+<div class="btn-group" style="width:100%">
+  <a href="index.php"><button style="width:16.663%">Inicio</button></a>
+  <a href="eventos.php"><button style="width:16.663%">Eventos</button></a>
+  <a href="menu.php"><button style="width:16.663%">Menu</button></a>
+  <a href="Servicios.php"><button style="width:16.663%">Servicios</button></a>
+  <a href="login.php"><button style="width:16.663%">Iniciar Sesion</button></a>
+  <a href="contacto.php"><button style="width:16.663%">Contacto</button></a>
+</div>
+
+<!-- Parte a editar -->
+<br></br>
+
+	<body>
+		
+<html>
+<head>
+  <title>Buffet</title>
+  <link href="https://https://fonts.google.com/specimen/Squada+One :600,900" rel="stylesheet">
+</head>
+<body>
+  <div class="fuente-1">
+  <p class="heading">Buffet</p>
+</div>
+  <div class="gallery-image">
+    <div class="img-box">
+      <img src="img/entrada.jpg" alt="" />
+      <div class="transparent-box">
+        <div class="caption">
+          <p>Comidas de entrada</p>
+          <p class="opacity-low">Tipo de entradas a sus eventos</p>
+        </div>
+      </div> 
+    </div>
+    <div class="img-box">
+      <img src="img/fuerte.jpg" alt="" />
+      <div class="transparent-box">
+        <div class="caption">
+          <p>Comidas fuertes</p>
+          <p class="opacity-low">Comidas fuertes para sus eventos</p>
+        </div>
+      </div>
+    </div>
+    <div class="img-box">
+      <img src="img/postre.jpg" alt="" />
+      <div class="transparent-box">
+        <div class="caption">
+          <p>Postres</p>
+          <p class="opacity-low">Deliciosos postres para sus eventos</p>
+        </div>
+      </div>
+    </div>
+    <div class="img-box">
+      <img src="img/Ponque.jpg" alt="" />
+      <div class="transparent-box">
+        <div class="caption">
+          <p>Ponques</p>
+          <p class="opacity-low">Pasteles y ponques para sus eventos especiales</p>
+        </div>
+      </div> 
+    </div>
+    <div class="img-box">
+      <img src="img/bebidas.jpg" alt="" />
+      <div class="transparent-box">
+        <div class="caption">
+          <p>Bebidas</p>
+          <p class="opacity-low">Bebidas para cualquier ocación</p>
+        </div>
+      </div> 
+    </div>
+      <div class="img-box">
+      <img src="img/otros.jpg" alt="" />
+      <div class="transparent-box">
+        <div class="caption">
+          <p>Otras opciones</p>
+          <p class="opacity-low">Comidas opcionales que usted quisiera solicitar</p>
+        </div>
+      </div>
+    </div>
+      </div> 
+    </div>
+  </div>
+</body>
+</html>
+
+	</body>
+<!-- Fin de la parte a editar -->
+
+ 
+<footer class="footer-distributed">
+ 
+			<div class="footer-left">
+          <img src="img/elite.png" style="width: 18%">
+				<h3>Euro<span>Bodas</span></h3>
+ 
+				<p class="footer-links">
+					<a href="index.php">Inicio</a>
+					|
+					<a href="nosotros.php">Sobre Nosotros</a>
+					|
+					<a href="#">About</a>
+					|
+					<a href="#">Contacto</a>
+				</p>
+ 
+				<p class="footer-company-name">© 2019 EuroBodas Derechos reservados </p>
+			</div>
+ 
+			<div class="footer-center">
+				<div>
+					<i class="fa fa-map-marker"></i>
+					  <p><span>A 22b-71,, Cra. 44a #22b1</span>
+						Colombia, Bogota - 110111</p>
+				</div>
+ 
+				<div>
+					<i class="fa fa-phone"></i>
+					<p> (+57) 321 477 1511 </p>
+				</div>
+				<div>
+					<i class="fa fa-envelope"></i>
+					<p><a href="mailto:comercial01.bodaselite@gmail.com">comercial01.bodaselite@gmail.com</a></p>
+
+
+				</div>
+			</div>
+			<div class="footer-right">
+				<p class="footer-company-about">
+					<span>Más de nosotros</span>
+					Ofrecemos todo tipo de eventos, sociales, empresariales, y todo lo referente a cada uno.</p>
+				<div class="footer-icons">
+					<a href="#"><i class="fa fa-facebook"></i></a>
+					<a href="#"><i class="fa fa-instagram"></i></a>
+					<a href="#"><i class="fa fa-linkedin"></i></a>
+					<a href="#"><i class="fa fa-youtube"></i></a>
+				</div>
+			</div>
+		</footer>
+
+</body>
+
+</html>
