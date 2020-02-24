aa50ff656a256e3b699cbe3d4ff514bc764dfd1a
Camilo urrego <51963457+camilu0802@users.noreply.github.com>
2020-02-24 16:12:37 -0500

    Add files via upload

---
+++ contacto.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+ <title>Eurobodas </title>
+ <!-- Estilos chingue su madre -->
+ <head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="keywords" content="footer, address, phone, icons" />
+ 
+	<title>Responsive Footer</title>
+ 
+	<link rel="stylesheet" href="css/estilo.css">
+	
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+ 
+  <link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
+  
+  <link href="css/estilo.css" rel="stylesheet" type="text/css">
+</head>
+
+<div class="topnav">
+  <a href="nosotros.php">Nosotros</a>
+  <a href="Registro_cliente.php">Registro</a>
+  <div class="search-container">
+    <form action="/action_page.php">
+      <input type="text" placeholder="Search.." name="search">
+      <button type="submit">Buscar</button>
+    </form>
+  </div>
+</div>
+<div align="center">
+
+  <img src="img/elite.png" style="width: 20%">
+
+</div>
+
+
+
+
+<div class="btn-group" style="width:100%">
+  <a href="index.php"><button style="width:16.663%">Inicio</button></a>
+  <a href="eventos.php"><button style="width:16.663%">Eventos</button></a>
+  <a href="menu.php"><button style="width:16.663%">Menu</button></a>
+  <a href="Servicios.php"><button style="width:16.663%">Servicios</button></a>
+  <a href="login.php"><button style="width:16.663%">Iniciar Sesion</button></a>
+  <a href="contacto.php"><button style="width:16.663%">Contacto</button></a>
+</div>
+
+<!-- Parte a editar -->
+<br></br>
+
+<div class="contenedor" style="width: 100%">
+<img src="img/gogo.jpg" >
+<div id="header" align="center">
+<div class="texto-encima">
+<div class="centrado">
+<br></br>
+<br></br>
+<br></br>
+<br></br>
+  <a id="logo"><h1>Contactenos</h1></a>
+  <br></br>
+ <div class="form">
+
+		<a href="https://www.google.com/maps/place/Hotel+Macao/@4.6335276,-74.0969947,17z/data=!3m1!4b1!4m5!3m4!1s0x8e3f9bdc75270581:0xa016470a3b23bd13!8m2!3d4.6335276!4d-74.094806"><h3>Dirección - Cra. 44a N° 22-b-71 </h3></a>
+
+		<h3>Telefono - (+57) 321 477 1511</h3>
+		
+		<h3>Fijo - 1234567</h3>
+		
+		<h3>Correo - Eurobodas@Eurobodas.com</h3>
+		
+		<h2> Gracias por escogernos </h2>
+ </div>
+ </div>	
+ </div>
+</div>
+</div>
+</div>
+
+<!-- Fin de la parte a editar -->
+
+<br></br>
+ 
+
+
+
+  Texto
+  Centrado
